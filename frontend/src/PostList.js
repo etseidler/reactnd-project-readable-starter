a4f73ab730cc0048dc124e670c80e731a2779bb0
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-14 08:44:48 -0600

    refactor: use destructing to improve readability

--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -8,13 +8,14 @@ function PostList(props) {
   return (
     <div className="post-list">
       {postIds.length > 0
-        ? postIds.map(postId => (
-          <div key={postId} className="post-item">
-            <div className="post-vote-score">{postsById[postId].voteScore.toString().padStart(5)}</div>
-            <div className="post-title">{postsById[postId].title}</div>
-            <div className="post-comment-count">{postsById[postId].commentCount} comment(s)</div>
+        ? postIds.map((postId) => {
+          const { voteScore, title, commentCount } = postsById[postId]
+          return <div key={postId} className="post-item">
+            <div className="post-vote-score">{voteScore.toString().padStart(5)}</div>
+            <div className="post-title">{title}</div>
+            <div className="post-comment-count">{commentCount} comment(s)</div>
           </div>
-        ))
+        })
         :
         <NotFound text="No Posts Found" />
       }
