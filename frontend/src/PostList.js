2d8ae69a047fd1a596afb29c6c27bfa4340f0d73
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-15 08:33:45 -0600

    feature: add sorting to PostList

--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -3,12 +3,57 @@ import { connect } from 'react-redux'
 import NotFound from './NotFound'
 
 class PostList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      sortedIds: this.props.postIds,
+      sortOrder: ""
+    }
+
+    this.sortValueToSortFunction = {
+      'voteScore': this.sortDescending,
+      'title': this.sortAscending,
+      'timestampDescending': this.sortDescending,
+      'timestampAscending': this.sortAscending
+    }
+
+    this.handleSortChange = this.handleSortChange.bind(this)
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ sortedIds: nextProps.postIds })
+  }
+  sortDescending(first, second) {
+    return first > second ? -1 : 1
+  }
+  sortAscending(first, second) {
+    return first < second ? -1 : 1
+  }
+  handleSortChange({ target: { value: sortProp }}) {
+    const { postsById } = this.props
+    const sortedIds = this.state.sortedIds.sort((a, b) => {
+      const first = postsById[a][sortProp]
+      const second = postsById[b][sortProp]
+      return this.sortValueToSortFunction[sortProp](first, second)
+    })
+    this.setState({
+      sortedIds,
+      sortOrder: sortProp
+    })
+  }
   render() {
-    const { postIds, postsById } = this.props;
+    const { postsById } = this.props
     return (
       <div className="post-list">
-        {postIds.length > 0
-          ? postIds.map((postId) => {
+        <label>Sort By</label>
+        <select onChange={this.handleSortChange} value={this.state.sortOrder} >
+          <option value="voteScore">Vote Score</option>
+          <option value="timestampDescending">Date (newest first)</option>
+          <option value="timestampAscending">Date (oldest first)</option>
+          <option value="title">Title</option>
+        </select>
+        {this.state.sortedIds.length > 0
+          ? this.state.sortedIds.map((postId) => {
             const { voteScore, title, commentCount } = postsById[postId]
             return (
               <div key={postId} className="post-item">
