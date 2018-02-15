bc5a96c19e61f4e9fa0bbbbb55eb087cbeeb22fd
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-15 07:57:59 -0600

    refactor: add whitespace

--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -10,11 +10,13 @@ class PostList extends Component {
         {postIds.length > 0
           ? postIds.map((postId) => {
             const { voteScore, title, commentCount } = postsById[postId]
-            return <div key={postId} className="post-item">
-              <div className="post-vote-score">{voteScore.toString().padStart(5)}</div>
-              <div className="post-title">{title}</div>
-              <div className="post-comment-count">{commentCount} comment(s)</div>
-            </div>
+            return (
+              <div key={postId} className="post-item">
+                <div className="post-vote-score">{voteScore.toString().padStart(5)}</div>
+                <div className="post-title">{title}</div>
+                <div className="post-comment-count">{commentCount} comment(s)</div>
+              </div>
+            )
           })
           :
           <NotFound text="No Posts Found" />
