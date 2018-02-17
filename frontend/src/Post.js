d4c4ccd9ec426ea3b2a030c004f029f7f79330bb
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-17 14:00:18 -0600

    refactor: make Post a class component to prepare for voting

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Post({ post: { voteScore, title, commentCount } }) {
-  return (
-    <div className="post-item">
-      <div className="post-vote">
-      <div className="post-vote-score">{voteScore.toString().padStart(5)}</div>
-        <div className="post-vote-controls">
-          <i className="post-vote-icon icon ion-arrow-up-a"></i>
-          <i className="post-vote-icon icon ion-arrow-down-a"></i>
+class Post extends Component {
+  render() {
+    const { post: { voteScore, title, commentCount } } = this.props
+    return (
+      <div className="post-item">
+        <div className="post-vote">
+        <div className="post-vote-score">{voteScore.toString().padStart(5)}</div>
+          <div className="post-vote-controls">
+            <i className="post-vote-icon icon ion-arrow-up-a"></i>
+            <i className="post-vote-icon icon ion-arrow-down-a"></i>
+          </div>
         </div>
+        <div className="post-title">{title}</div>
+        <div className="post-comment-count">{commentCount} comment(s)</div>
       </div>
-      <div className="post-title">{title}</div>
-      <div className="post-comment-count">{commentCount} comment(s)</div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Post
