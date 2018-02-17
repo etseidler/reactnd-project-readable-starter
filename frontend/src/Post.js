9de06f3b044034da6991f38669e810c4b86949b9
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-17 13:54:20 -0600

    feature: create non-functional vote controls

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Post({ post: { voteScore, title, commentCount } }) {
   return (
     <div className="post-item">
+      <div className="post-vote">
       <div className="post-vote-score">{voteScore.toString().padStart(5)}</div>
+        <div className="post-vote-controls">
+          <i className="post-vote-icon icon ion-arrow-up-a"></i>
+          <i className="post-vote-icon icon ion-arrow-down-a"></i>
+        </div>
+      </div>
       <div className="post-title">{title}</div>
       <div className="post-comment-count">{commentCount} comment(s)</div>
     </div>
