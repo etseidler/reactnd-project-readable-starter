a496388a23fdcb78ade9e8605f5791b5791af12a
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-21 20:00:55 -0600

    feature: add author and change layout of single Post

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -11,7 +11,9 @@ class Post extends Component {
     upvotePost(id).then(() => this.props.upvote(id))
   }
   render() {
-    const { post: { id, voteScore, title, commentCount } } = this.props
+    const { post:
+      { id, voteScore, title, commentCount, author }
+    } = this.props
     return (
       <div className="post-item">
         <div className="post-vote">
@@ -21,8 +23,13 @@ class Post extends Component {
             <i className="post-vote-icon icon ion-arrow-down-a" onClick={() => this.downvote(id)} />
           </div>
         </div>
-        <div className="post-title">{title}</div>
-        <div className="post-comment-count">{commentCount} comment{commentCount !== 1 ? 's' : ''}</div>
+        <div className="post-main">
+          <div className="post-title">{title}</div>
+          <div className="post-minor">
+            <div className="post-comment-count">{commentCount} comment{commentCount !== 1 ? 's' : ''}</div>
+            <div className="post-author">by {author}</div>
+          </div>
+        </div>
       </div>
     )
   }
