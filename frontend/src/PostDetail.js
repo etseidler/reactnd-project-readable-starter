0aaf2428897ab9e17f784dafeed655015261b663
Eric Seidler <eric.seidler@asynchrony.com>
2018-02-28 12:25:12 -0600

    feature: change category when viewing a specific post

--- frontend/src/PostDetail.js
+++ frontend/src/PostDetail.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { changeCategory } from './actions'
 
-function PostDetail(props) {
-  const { title, body, author, commentCount, voteScore } = props.post
-  return (
-    <div className="post-detail">
-      <div className="post-detail-title">{title}</div>
-      <div className="post-detail-body">{body}</div>
-      <div className="post-detail-author">{author}</div>
-      <div className="post-detail-comment-count">{commentCount}</div>
-      <div className="post-detail-voteScore">{voteScore}</div>
-    </div>
-  )
+class PostDetail extends Component {
+  componentDidMount() {
+    this.props.changeCategory(this.props.post.category)
+  }
+  render() {
+    const { title, body, author, commentCount, voteScore } = this.props.post
+    return (
+      <div className="post-detail">
+        <div className="post-detail-title">{title}</div>
+        <div className="post-detail-body">{body}</div>
+        <div className="post-detail-author">{author}</div>
+        <div className="post-detail-comment-count">{commentCount}</div>
+        <div className="post-detail-voteScore">{voteScore}</div>
+      </div>
+    )
+  }
 }
 
-export default PostDetail
+function mapDispatchToProps(dispatch) {
+  return {
+    changeCategory: category => dispatch(changeCategory(category))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PostDetail)
